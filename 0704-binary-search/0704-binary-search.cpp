894f37e959279a4d43acdc2b7a6c515e4eb18c0e
Zeus3252 <33201512+Zeus3252@users.noreply.github.com>
2022-10-27 01:08:43 -0400

    Time: 129 ms (5.42%), Space: 27.5 MB (92.27%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) { 
+        
+    int arrSize = nums.size();
+    int i = 0;
+    
+    while (i < arrSize) {
+        if (nums[i] != target){
+        i++;
+     }else if (nums[i] == target){
+        return i;  
+     }  
+  }  
+    return -1;
+ }
+};
