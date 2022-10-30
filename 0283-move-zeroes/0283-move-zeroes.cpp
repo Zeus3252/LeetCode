0a58d73cbf1f5cfec1443e3c06f266e87d1fba5f
Zeus3252 <33201512+Zeus3252@users.noreply.github.com>
2022-10-30 03:40:45 -0400

    Time: 66 ms (23.77%), Space: 19.2 MB (83.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -7,8 +7,8 @@ public:
        int arrSize = nums.size();
         //cout << arrSize;
         while (y < arrSize){
-            if (nums[i] == 0 && nums[y] != 0) {
-                swap(nums[i], nums[y]);
+        if (nums[i] == 0 && nums[y] != 0) {
+            swap(nums[i], nums[y]);
                 i++;
                 y++;
 
@@ -17,9 +17,9 @@ public:
                 y++;
 
         } else if (nums[i] == 0 && nums[y] == 0) {
-               
                 y++;
-        } else {
+            
+        } else if (nums[i] != 0 && nums[y] != 0){
                i++;
                y++; 
             }
