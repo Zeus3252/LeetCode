5ed066b22ec5022042f5cdb1f230bc7cc6778a64
Zeus3252 <33201512+Zeus3252@users.noreply.github.com>
2022-10-30 02:11:06 -0400

    Time: 68 ms (20.72%), Space: 19.3 MB (57.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0;
+        int x = 0;
+        int y = 1;
+       int arrSize = nums.size();
+        //cout << arrSize;
+        while (y < arrSize){
+            if (nums[i] == 0 && nums[y] != 0) {
+                swap(nums[i], nums[y]);
+                i++;
+                y++;
+   
+        } else if (nums[i] == 0 && nums[y] == 0) { 
+                y++;
+        } else {
+                i++;
+                y++;
+
+    }
+        
+    }
+    }
+};
