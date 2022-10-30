4d1a555d2265098a9bf4ae62f5faa71d3942e098
Zeus3252 <33201512+Zeus3252@users.noreply.github.com>
2022-10-30 02:47:27 -0400

    Time: 45 ms (64.29%), Space: 19.2 MB (83.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -7,19 +7,16 @@ public:
        int arrSize = nums.size();
         //cout << arrSize;
         while (y < arrSize){
-            if (nums[i] == 0 && nums[y] != 0) {
-                swap(nums[i], nums[y]);
+            if (nums[i] == 0 && nums[y] != 0) { //Comparing 0,1, swap digits
+                swap(nums[i], nums[y]); //swap values at indexes
                 i++;
                 y++;
-   
-        } else if (nums[i] == 0 && nums[y] == 0) { 
+        } else if (nums[i] == 0 && nums[y] == 0) {//Compare 0,1, traverse second pointer y 
                 y++;
-        } else {
+        } else { //Compare 1,0, traverse both pointers
                 i++;
                 y++;
-
-    }
-        
-    }
+          } 
+       }
     }
 };
